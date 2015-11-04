609ed558ae6d7a090336e967796ea3babdb5aa52
ariel <alvarez.ariel.work@gmail.com>
2015-11-04 19:48:19 -0300

    KILL IT WITH FIRE

--- core/src/gdx/scala/demo/GdxScalaDemoGame.scala
+++ core/src/gdx/scala/demo/GdxScalaDemoGame.scala
@@ -2,24 +2,60 @@ package gdx.scala.demo
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.VertexAttributes.Usage
+import com.badlogic.gdx.graphics._
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
-class GdxScalaDemoGame extends ApplicationAdapter {
-  private[demo] var batch: SpriteBatch = null
-  private[demo] var img: Texture = null
+abstract class GdxScalaDemoGame extends ApplicationAdapter {
+  var squareMesh:Mesh
+  var camera:OrthographicCamera
+  var texture:Texture
+  var spriteBatch:SpriteBatch
 
   override def create() {
-    batch = new SpriteBatch
-    img = new Texture("badlogic.jpg")
+    if (squareMesh == null) {
+      squareMesh = new Mesh(true, 4, 4,
+        new VertexAttribute(Usage.Position, 3, "a_position"),
+        new VertexAttribute(Usage.ColorPacked, 4, "a_color"))
+
+
+      squareMesh.setVertices(Array[Float](
+        -0.5f, -0.5f, 0, Color.toFloatBits(128, 0, 0, 255),
+        0.5f, -0.5f, 0, Color.toFloatBits(192, 0, 0, 255),
+        -0.5f, 0.5f, 0, Color.toFloatBits(192, 0, 0, 255),
+        0.5f, 0.5f, 0, Color.toFloatBits(255, 0, 0, 255) ))
+
+      squareMesh.setIndices(Array[Short]( 0, 1, 2, 3))
+    }
+
+    texture = new Texture(Gdx.files.internal("data/badlogic.jpg"))
+    spriteBatch = new SpriteBatch()
   }
 
-  override def render() {
-    Gdx.gl.glClearColor(1, 0, 0, 1)
-    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-    batch.begin()
-    batch.draw(img, 0, 0)
-    batch.end()
-  }
+
+//  override def render() {
+//    //    handleInput();
+//    camera.update()
+//    spriteBatch.setProjectionMatrix(camera.combined)
+//
+//    Gdx.gl.glDepthMask(false);
+//
+//    //enable blending, for alpha
+//    Gdx.gl.glEnable(GL20.GL_BLEND);
+//    Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+//
+//
+//    squareMesh.render(GL20.GL_TRIANGLE_STRIP, 0, 4)
+//
+//    spriteBatch.begin();
+//    spriteBatch.draw(texture, 0, 0, 1, 1, 0, 0,
+//      texture.getWidth(), texture.getHeight(), false, false)
+//    spriteBatch.end()
+//  }
+//
+//  override def resize(width:Int, height:Int) {
+//    float aspectRatio = (float) width / (float) height;
+//    camera = new OrthographicCamera(2f * aspectRatio, 2f);
+//  }
+
 }
--- desktop/src/gdx/scala/demo/desktop/DesktopLauncher.java
+++ desktop/src/gdx/scala/demo/desktop/DesktopLauncher.java
@@ -3,10 +3,12 @@ package gdx.scala.demo.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import gdx.scala.demo.GdxScalaDemoGame;
+import gdx.scala.demo.MeshTutorial1;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new GdxScalaDemoGame(), config);
+//		new LwjglApplication(new GdxScalaDemoGame(), config);
+		new LwjglApplication(new MeshTutorial1(), config);
 	}
 }
