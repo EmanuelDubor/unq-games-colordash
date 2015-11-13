b6036af5e84d4c94c0549369221f5d7788a337ef
EmanuelDubor <emanueldubora@hotmail.com>
2015-11-13 14:48:26 -0300

    Removed old shader traits.

--- core/src/gdx/scala/colordash/ColorDashGame.scala
+++ core/src/gdx/scala/colordash/ColorDashGame.scala
@@ -1,30 +1,25 @@
 package gdx.scala.colordash
 
 import com.badlogic.gdx.graphics._
-import com.badlogic.gdx.graphics.glutils.ShaderProgram
 import com.badlogic.gdx.{ApplicationAdapter, Gdx}
 
-class ColorDashGame extends ApplicationAdapter with ShaderRenderer {
-  var camera:OrthographicCamera = _
-  var shader:ShaderProgram = _
-  var mesh: Mesh = _
+class ColorDashGame extends ApplicationAdapter {
+  var camera: OrthographicCamera = _
 
-  var player:Player = _
+  var player: Player = _
 
   override def create() {
     camera = new OrthographicCamera()
-    mesh = createMesh()
-    shader = createMeshShader()
 
     TiledWorld.initialize()
 
     player = new Player()
-    camera.setToOrtho(false, 20,15)
+    camera.setToOrtho(false, 20, 15)
     camera.update()
   }
 
   override def render() {
-    Gdx.gl.glClearColor(110/255f, 110/255f, 110/255f, 1)
+    Gdx.gl.glClearColor(110 / 255f, 110 / 255f, 110 / 255f, 1)
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
     camera.update()
@@ -33,17 +28,13 @@ class ColorDashGame extends ApplicationAdapter with ShaderRenderer {
     camera.position.x = player.rect.x
     camera.position.y = player.rect.y
 
-    TiledWorld.render(player,camera)
+    TiledWorld.render(player, camera)
 
   }
 
-  override def resize(width:Int, height:Int) {
-//    val aspectRatio =  width / height
-//    camera = new OrthographicCamera(2f * aspectRatio, 2f)
+  override def resize(width: Int, height: Int) {
+    //    val aspectRatio =  width / height
+    //    camera = new OrthographicCamera(2f * aspectRatio, 2f)
   }
 
-  override def dispose() {
-    mesh.dispose()
-    shader.dispose()
-  }
 }
--- core/src/gdx/scala/colordash/SpriteRenderer.scala
+++
@@ -1,5 +0,0 @@
-package gdx.scala.colordash
-
-trait SpriteRenderer {
-
-}
